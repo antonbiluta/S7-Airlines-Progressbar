a8b5edd9caf70f7de7be2703cde83d94680683d4
GitHub Action <action@github.com>
2023-09-17 16:48:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/antonbiluta/s7airlinesprogressbar/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/antonbiluta/s7airlinesprogressbar/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.antonbiluta.s7airlinesprogressbar.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
